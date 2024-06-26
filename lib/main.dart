edc8af02919d4f6e1d8a602a209b18ca2f6d2000
Jhonny <me.is.intik@gmail.com>
2024-06-26 22:05:49 +0100

    added firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,11 +14,23 @@ import 'package:trivico/core/providers/onboarding_notifier_provider.dart';
 import 'package:trivico/core/providers/shared_prefrences_provider.dart';
 import 'package:trivico/core/providers/theme_notifier_provider.dart';
 import 'package:trivico/core/utils/theme.dart';
+import 'package:trivico/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final sharedPreferences = await SharedPreferences.getInstance();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  if (kReleaseMode) {
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
   runApp(
     ProviderScope(
       overrides: [
